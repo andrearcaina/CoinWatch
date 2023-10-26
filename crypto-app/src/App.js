7d1b32ea49c485eac69491622114c0b3a34725d8
Andre <dtandre331@gmail.com>
2023-10-26 17:42:00 -0400

    imported necessary antd css
    - fixed footer
    - fixed indentation
    - implemented proper formatting

--- crypto-app/src/App.js
+++ crypto-app/src/App.js
@@ -16,25 +16,25 @@ const App = () => {
                 <Layout>
                     <div className='routes'> 
                         <Routes>
-                        <Route exact path="/" element={<Homepage/>}/>
-                        <Route exact path="/cryptocurrencies" element={<Cryptocurrencies/>}/>
-                        <Route exact path="/crypto/:coinId" element={<CryptoDetails/>}/>
-                        <Route exact path="/news" element={<News/>}/>
+                            <Route exact path="/" element={<Homepage/>}/>
+                            <Route exact path="/" element={<Exchanges/>}/>
+                            <Route exact path="/cryptocurrencies" element={<Cryptocurrencies/>}/>
+                            <Route exact path="/cryptocurrencies/:coinId" element={<CryptoDetails/>}/>
+                            <Route exact path="/news" element={<News/>}/>
                         </Routes>
                     </div>
                 </Layout>
-            </div>
-
-            <div className="footer">
-                <Typography.Title level={5} style={{color:'white', textAlign:'center'}}>
-                    CryptoVest<br/>
-                    by <a href="https://github.com/andrearcaina" target="_blank" rel="noreferrer" style={{color:"#A5C9CA"}}>andrearcaina</a>
-                </Typography.Title>
-                <Space>
-                    <Link to="/">Home</Link>
-                    <Link to="/exchanges">Exchanges</Link>
-                    <Link to="/news">News</Link>
-                </Space>
+                <div className="footer">
+                    <Typography.Title level={5} style={{color:'white', textAlign:'center'}}>
+                        CoinWatch<br/>
+                        by <a href="https://github.com/andrearcaina" target="_blank" rel="noreferrer" style={{color:"#A5C9CA"}}>andrearcaina</a>
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
+                </div>
             </div>
         </div>
     )
--- crypto-app/src/index.js
+++ crypto-app/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+import 'antd/dist/reset.css';
+import reportWebVitals from './reportWebVitals';
 
 // Render the App component into the root element.
-ReactDOM.render(
+const ROOT = ReactDOM.createRoot(document.getElementById('root'));
+
+ROOT.render(
     <Router>
         <App />
-    </Router>, 
-    document.getElementById('root'));
+    </Router>
+);
 
+reportWebVitals();
