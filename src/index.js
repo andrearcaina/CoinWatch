75a707320d2aea16422f2c751028bb24972001fc
Andre <dtandre331@gmail.com>
2023-10-25 21:32:19 -0400

    added src folder and initial code

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+// Render the App component into the root element.
+ReactDOM.render(<App />, document.getElementById('root'));
+
