1bf4368f25407fb9855b1357fad4d4cfeef35d5e
Andre <dtandre331@gmail.com>
2023-10-26 17:30:05 -0400

    implemented main nav
    - homepage
    - exchanges
    - cryptocurrencies
    - cryptodetails
    - news

--- crypto-app/src/App.js
+++ crypto-app/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar } from './components';
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
 import './styles.css';
 
 const App = () => {
@@ -13,11 +13,28 @@ const App = () => {
             </div>
             
             <div className="main">
-
+                <Layout>
+                    <div className='routes'> 
+                        <Routes>
+                        <Route exact path="/" element={<Homepage/>}/>
+                        <Route exact path="/cryptocurrencies" element={<Cryptocurrencies/>}/>
+                        <Route exact path="/crypto/:coinId" element={<CryptoDetails/>}/>
+                        <Route exact path="/news" element={<News/>}/>
+                        </Routes>
+                    </div>
+                </Layout>
             </div>
 
             <div className="footer">
-
+                <Typography.Title level={5} style={{color:'white', textAlign:'center'}}>
+                    CryptoVest<br/>
+                    by <a href="https://github.com/andrearcaina" target="_blank" rel="noreferrer" style={{color:"#A5C9CA"}}>andrearcaina</a>
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to="/news">News</Link>
+                </Space>
             </div>
         </div>
     )
